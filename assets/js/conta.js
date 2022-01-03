36513a3e8faf180e18eedd9b2c7cd604a8b12c8e
ELHADRINadia <93933520+ELHADRINadia@users.noreply.github.com>
2022-01-03 17:08:07 +0100

    modifier js

---
+++ assets/js/conta.js
@@ -0,0 +1,11 @@
+function verfier() {
+    var elts = document.querySelectorAll('input');
+    var name = document.getElementById('C1').value;
+    var Email = document.getElementById('C2').value;
+    var Tel = document.getElementById('C3').value;
+    var message = document.getElementById('C4').value;
+    if (elts[0].value != "" && elts[1].value != "" && elts[2].value != "" && elts[3].value != "") {
+        alert("Bonjour" + name + "/n" + Email + "/n" + Tel + "/n" + message);
+    } else
+        alert("rempler votre champs");
+}
